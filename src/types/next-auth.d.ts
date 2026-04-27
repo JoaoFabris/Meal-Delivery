ff881bfa4944c7910ddd4ee7617e735915e292c2
fabris <jvfabris21@gmail.com>
2026-04-27 18:31:45 -0300

    refactor: update Prisma client initialization and enhance NextAuth type definitions
    - Simplified Prisma client initialization by removing unnecessary imports and adjusting log levels.
    - Enhanced NextAuth type definitions to include optional user properties and role management for better type safety.

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,16 +1,26 @@
-import NextAuth, { DefaultSession } from 'next-auth'
-import { JWT } from 'next-auth/jwt'
+import 'next-auth'
+import 'next-auth/jwt'
 
 declare module 'next-auth' {
   interface Session {
     user: {
+      id?: string
+      name?: string | null
+      email?: string | null
+      image?: string | null
       isAdmin: boolean
-    } & DefaultSession['user']
+      role: string
+    }
+  }
+
+  interface User {
+    role?: string
   }
 }
 
 declare module 'next-auth/jwt' {
   interface JWT {
     isAdmin: boolean
+    role: string
   }
 }
