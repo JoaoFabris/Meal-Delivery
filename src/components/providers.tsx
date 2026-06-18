ea4ede759d3634f3a3e9cc866f518d374d7e1a03
fabris <jvfabris21@gmail.com>
2026-06-18 19:14:09 -0300

    feat: add FavoritesSyncer component to manage user favorites based on session state

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,11 +1,31 @@
 'use client'
 
-import { SessionProvider } from 'next-auth/react'
+import { SessionProvider, useSession } from 'next-auth/react'
 import { Toaster } from '@/components/ui/sonner'
+import { useEffect } from 'react'
+import { useFavoritesStore } from '@/lib/store/favorites.store'
+
+function FavoritesSyncer() {
+    const { data: session } = useSession()
+    const { setFavoriteIds, clearFavorites } = useFavoritesStore()
+
+    useEffect(() => {
+        if (!session?.user?.email) {
+            clearFavorites()
+            return
+        }
+        fetch('/api/favorites')
+            .then(res => res.json())
+            .then(data => setFavoriteIds(data.favorites ?? []))
+    }, [session?.user?.email])
+
+    return null
+}
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <SessionProvider>
+            <FavoritesSyncer />
             {children}
             <Toaster position="top-right" richColors />
         </SessionProvider>
